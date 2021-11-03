ef7f6316db0d2e18dc760afa72a6a3d1f8fb8931
monstrillllo <evgeniy27112000@gmail.com>
2021-11-03 14:28:15 +0300

    3 random types

---
+++ main.py
@@ -0,0 +1,39 @@
+from random import randbytes, randint
+from os import urandom
+import math
+
+
+def default_random(length: int = 100) -> bytes:
+    return randbytes(length)
+
+
+def crypto_random(length: int = 100) -> bytes:
+    return urandom(length)
+
+
+def generator(length: int = 100):
+    res = []
+    xt = randint(0, 9)
+    xt1 = randint(0, 9)
+    xt2 = randint(0, 9)
+    for _ in range(length):
+        x = (1176 * xt + 1476 * xt1 + 1776 * xt2) % (2 ** 32 - 5)
+        xt2, xt1, xt = xt1, xt, x
+        res.append(x.to_bytes(math.ceil(math.log2(x) / 8), 'big'))
+    return res
+
+
+def main():
+    with open('./result1', 'wb') as file:
+        file.write(default_random(200))
+        # file.write(crypto_random(200))
+        # file.write(generator(200))
+        print('Done')
+    # print(default_random(200))
+    # print(crypto_random(200))
+    # print(generator(200))
+
+
+if __name__ == "__main__":
+    main()
+
